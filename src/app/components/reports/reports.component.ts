f469897b47866ce7a4be5982e0656f5e9b5c6549
AatreshKulkarni <aatresh.kulkarni08@gmail.com>
2018-08-10 16:26:21 +0530

    added some data

--- src/app/components/reports/reports.component.ts
+++ src/app/components/reports/reports.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit , ViewChild} from '@angular/core';
 import { ConnectorService } from './../../services/connector.service';
 import { ExcelService } from './../../services/excel.service';
 import { MatTableDataSource, MatPaginator } from '@angular/material';
+import {IMyDpOptions} from 'mydatepicker';
 
 @Component({
   selector: 'app-reports',
@@ -12,16 +13,22 @@ import { MatTableDataSource, MatPaginator } from '@angular/material';
 })
 export class ReportsComponent implements OnInit {
 
-  dcFlag = true;
-  hwcFlag = false;
-  publicityFlag = false;
+  public myDatePickerOptions: IMyDpOptions = {
+    // other options...
+    dateFormat: 'yyyy-mm-dd',
+};
 
+  fromDate: any;
+  toDate: any;
+  case:any = "case1";
+  caseList:any= ["case1", "case2"]
   record: any;
   dataSource: any;
   @ViewChild(MatPaginator) paginator: MatPaginator;
   totalPost = 10;
   postPerPage = 10;
   pageSizeOptions = [5, 10, 20, 50, 100];
+
   constructor(private wildService: ConnectorService, private excelService: ExcelService) { }
 
   displayedCol = [
@@ -34,23 +41,71 @@ export class ReportsComponent implements OnInit {
   ];
 
   ngOnInit() {
-    this.record = this.wildService.getReport();
-    this.record.subscribe(res => {
-      if (!res) {
-        return;
-      }
-      this.dataSource = new MatTableDataSource(res);
-      this.dataSource.paginator = this.paginator;
-    });
+
   }
 
-  xlsxReport() {
+  xlsxReport(data) {
+if(this.fromDate != null && this.toDate !=null){
+
+  // var fday = this.fromDate.getDate();
+  // var fmonthIndex = this.fromDate.getMonth();
+  // var fyear = this.fromDate.getFullYear();
+  // var tday = this.toDate.getDate();
+  // var tmonthIndex = this.toDate.getMonth();
+  // var tyear = this.toDate.getFullYear();
+  // this.record = this.wildService.getDcByRange(fyear+"-"+fmonthIndex+"-"+fday, tyear+"-"+tmonthIndex+"-"+tday);
+  this.record = this.wildService.getDcByRange(this.fromDate.formatted, this.toDate.formatted);
+
+  this.record.subscribe(res => {
+    if (!res) {
+      return;
+    }
+    this.dataSource = new MatTableDataSource(res);
+    this.dataSource.paginator = this.paginator;
     if(this.dataSource.data){
       this.excelService.exportReport(this.dataSource.data,  'Reports');
       return 'success';
     }else{
       alert("No data to export");
     }
+  });
+}else if(this.case == "case1"){
+  this.record = this.wildService.getDcByCase1();
+  this.record.subscribe(res => {
+    if (!res) {
+      return;
+    }
+    this.dataSource = new MatTableDataSource(res);
+    this.dataSource.paginator = this.paginator;
+    if(this.dataSource.data){
+      this.excelService.exportReport(this.dataSource.data,  'Reports');
+      return 'success';
+    }else{
+      alert("No data to export");
+    }
+  });
+}
+else{
+
+  this.record = this.wildService.getDcByCase2();
+  this.record.subscribe(res => {
+    if (!res) {
+      return;
+    }
+    this.dataSource = new MatTableDataSource(res);
+    this.dataSource.paginator = this.paginator;
+    if(this.dataSource.data){
+      this.excelService.exportReport(this.dataSource.data,  'Reports');
+      return 'success';
+    }else{
+      alert("No data to export");
+    }
+  });
+}
+
+
+
+
 
   }
 
--- src/app/components/reports/reports.module.ts
+++ src/app/components/reports/reports.module.ts
@@ -6,13 +6,9 @@ import { MatToolbarModule, MatTableModule, MatFormFieldModule, MatInputModule,
   MatSidenavModule, MatButtonModule, MatIconModule, MatListModule, MatCardModule,
   MatPaginatorModule, MatSortModule, MatGridListModule, MatMenuModule, MatTabsModule,
   MatButtonToggleModule } from '@angular/material';
-
   import { MyDatePickerModule } from 'mydatepicker';
-
-
   import{FormsModule} from '@angular/forms'
   import {CommonModule} from '@angular/common';
-
 @NgModule({
   imports: [
 
@@ -22,8 +18,8 @@ MatSidenavModule, MatButtonModule, MatIconModule, MatListModule, MatCardModule,
 MatPaginatorModule, MatSortModule, MatGridListModule, MatMenuModule, MatTabsModule,
 MatButtonToggleModule,
 MyDatePickerModule,
-FormsModule,
 CommonModule,
+FormsModule
   ],
   declarations: [ ReportsComponent ]
 })
