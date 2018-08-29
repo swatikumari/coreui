baf8820855f26fab0f611f557a504b6f7df360d6
chethansrgit <chethanctn1@gmail.com>
2018-08-29 11:08:44 +0530

    charts

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -14,6 +14,36 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
   }
+  // lineChart
+  public lineChartData: Array<any> = [
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'BP'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'NH'}
+  ];
+  public lineChartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartOptions: any = {
+    animation: false,
+    responsive: true
+  };
+  public lineChartColours: Array<any> = [
+    { // grey
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'rgba(148,159,177,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    },
+    { // dark grey
+      backgroundColor: 'rgba(77,83,96,0.2)',
+      borderColor: 'rgba(77,83,96,1)',
+      pointBackgroundColor: 'rgba(77,83,96,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(77,83,96,1)'
+    }
+  ];
+  public lineChartLegend = true;
+  public lineChartType = 'line';
 // barChart
 public barChartOptions: any = {
   scaleShowVerticalLines: false,
